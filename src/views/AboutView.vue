f57b307291ef2bcd1b308521589bee01aa9a33e0
suman dulal <dulalsuman853@gmail.com>
2023-11-29 15:28:58 +0545

    update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class=" container">
+    <h1>hello sss</h1>
     <h1 class="text-3xl  text-red-700 md:text-green-600 font-bold underline">This is an about page</h1>
   </div>
 </template>
